e25c48dac8d4b1334af902e60e72711a22a51349
Eka <6597211+ekafyi@users.noreply.github.com>
2022-06-12 01:13:37 +0700

    feat: improve slide sample component

--- src/components/MySvelteSplide.svelte
+++ src/components/MySvelteSplide.svelte
@@ -1,3 +1,9 @@
+<!-- 
+	@component
+	Sample slide/carousel component with Svelte Splide
+	@link https://github.com/Splidejs/svelte-splide
+	@link https://splidejs.com/guides/options
+ -->
 <script>
 	import { Splide, SplideSlide } from "@splidejs/svelte-splide";
 	import "@splidejs/svelte-splide/css";
@@ -8,11 +14,21 @@
 		"https://images.unsplash.com/photo-1649330944677-427f4e9d3c4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800";
 </script>
 
-<Splide options={{ rewind: true }} aria-label="Svelte Splide Example">
+<Splide
+	options={{ rewind: true, heightRatio: 0.5 }}
+	aria-label="Astro Svelte Splide example"
+	class="my-svelte-splide"
+>
 	<SplideSlide>
-		<img src={img1Src} alt="" />
+		<img class="object-cover" src={img1Src} alt="" />
 	</SplideSlide>
 	<SplideSlide>
-		<img src={img2Src} alt="" />
+		<img class="object-cover" src={img2Src} alt="" />
 	</SplideSlide>
 </Splide>
+
+<style>
+	:global(.my-svelte-splide .splide__slide) {
+		background: #000;
+	}
+</style>
